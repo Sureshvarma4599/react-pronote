ad81813259e5faf76d326e079ed79c2b3479a810
Sureshvarma4599 <sureshvarma.n666@gmail.com>
2020-11-21 10:17:58 +0530

    e

---
+++ pronote/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Editor from './component/editor.jsx';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+   <Editor/>
+
+  );
+}
+
+export default App;
